7e2782a1aabb615fc11e6301ea4b1cbce87a4f72
Anastasia <anast111antonova@gmail.com>
2025-09-06 16:49:40 +0300

    Fix bugs; add another version of recursion

--- src/index.js
+++ src/index.js
@@ -1,29 +1,45 @@
 'use stirct'
+debugger
 
-let number = 1
+// function getNumber(number) {
+// 	if(number === 1) {
+// 		return '1'
+// 	}
+// 	if(number < 1) {
+// 		return null
+// 	}
 
-function getNumber(number) {
-	if(number == 1) {
-		return '1'
-	} else if(number < 1) {
-		return null
-	}
-
-	let result
+// 	let result
 	
-	result = getNumber(number - 5)
-	if(result != null) {
-		return '(' + result + ' + 5)'
-	}
+// 	result = getNumber(number - 5)
+// 	if(result !== null) {
+// 		return '(' + result + ' + 5)'
+// 	}
 
-	if(number % 3 == 0) {
-		result = getNumber(number / 3)
-		if(result != null) {
-			return '(' + result + ' * 3)'
+// 	if(number % 3 === 0) {
+// 		result = getNumber(number / 3)
+// 		if(result !== null) {
+// 			return '(' + result + ' * 3)'
+// 		}
+// 	}
+
+// 	return null
+// }
+
+function getNumber(target) {
+	function find(current, string) {
+		if(current === target) {
+			return string
 		}
+		if(current > target) {
+			return null
+		}
+		return (
+			find(current + 5, `(${string} + 5)`) || 
+			find(current * 3, `(${string} * 3)`)
+		)
 	}
-
-	return null
+	return find(1, '1')
 }
 
 console.log(getNumber(13));
